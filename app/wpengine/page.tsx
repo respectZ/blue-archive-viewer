6e0e018ee8a7d4175b9e4cfcb35387a0d09e0888
Svdex <ucihaibna@yahoo.com>
2024-02-04 20:55:49 +0700

    feat: various fix for configs

--- app/wpengine/page.tsx
+++ app/wpengine/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Live2DViewer } from "@/app/lib/live2d_viewer";
-import { useEffect } from "react";
+import { Live2DViewer } from "@/app/lib/live2d_wallpaper_engine";
+import { useEffect, useRef, useState } from "react";
 import * as Table from "@/app/lib/table";
 import { get_origin } from "@/app/lib/get_origin";
 
@@ -20,76 +20,128 @@ async function fetchModels(file: string): Promise<Record<string, string[]>> {
 }
 
 export default function Home() {
+  const [models, setModels] = useState<Record<string, string[]>>({});
+
+  const [options, setOptions] = useState({
+    selectedModel: "airi0_home",
+    scale: 0.8,
+    playVoice: false,
+    voiceVolume: 1,
+    tapToTalk: false,
+    maxFPS: 60,
+  });
+
+  // You can update individual properties using setOptions
+  const updateSelectedModel = (newModel: string) => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      selectedModel: newModel,
+    }));
+  };
+
+  const updateScale = (newScale: number) => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      scale: newScale,
+    }));
+  };
+
+  const updatePlayVoice = (newPlayVoice: boolean) => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      playVoice: newPlayVoice,
+    }));
+  };
+
+  const updateVoiceVolume = (newVoiceVolume: number) => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      voiceVolume: newVoiceVolume,
+    }));
+  };
+
+  const updateTapToTalk = (newTapToTalk: boolean) => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      tapToTalk: newTapToTalk,
+    }));
+  };
+
+  const updateMaxFPS = (newMaxFPS: number) => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      maxFPS: newMaxFPS,
+    }));
+  };
+
   useEffect(() => {
-    let options: Record<string, string | number | boolean> = {
-      selectedModel: "airi0",
-      scale: 0.8,
-      playVoice: false,
-      voiceVolume: 1,
-      maxFPS: 30,
-    };
-    let models: Record<string, string[]> = {};
     // @ts-ignore
     window.wallpaperPropertyListener = {
       applyUserProperties: async function (properties: Record<string, any>) {
-        if (properties.fps) {
-          options.maxFPS = properties.fps.value as number;
-          live2d.app.ticker.maxFPS = options.maxFPS;
-        }
         if (properties.play_voice) {
-          options.playVoice = properties.play_voice.value as boolean;
+          updatePlayVoice(properties.play_voice.value);
           live2d.playVoice = properties.play_voice.value;
         }
+        if (properties.tap_to_talk) {
+          updateTapToTalk(properties.tap_to_talk.value);
+        }
         if (properties.voice_volume) {
-          options.voiceVolume = (properties.voice_volume.value as number) / 100;
-          live2d.voiceVolume = properties.voice_volume.value;
+          updateVoiceVolume(properties.voice_volume.value / 100);
+          live2d.voiceVolume = properties.voice_volume.value / 100;
         }
         if (properties.model) {
-          options.selectedModel = properties.model.value as string;
-          const model = models[options.selectedModel as string];
+          if (options.selectedModel !== properties.model.value as string) {
+            updateSelectedModel(properties.model.value as string);
+            if (Object.keys(models).length > 0 && live2d) {
+              const model = models[options.selectedModel as string];
 
-          for (let i = 0; i < model.length; i++) {
-            const x = model[i];
-            if (i == 0) await live2d.loadModel(x);
-            else await live2d.addSpine(x, i.toString(), 0);
+              for (let i = 0; i < model.length; i++) {
+                const x = model[i];
+                if (i == 0) await live2d.loadModel(x);
+                else await live2d.addSpine(x, i.toString(), 0);
+              }
+
+              live2d.center();
+              live2d.scale(options.scale as number);
+              live2d.start();
+            }
           }
-
-          live2d.center();
-          live2d.scale(options.scale as number);
-          live2d.playAnimation("Start_Idle_01", {
-            onComplete: () => {
-              if (live2d.getAnimation("Idle_01") == null) return;
-
-              live2d.playAnimation("Idle_01");
-              live2d.loopAnimation = true;
-            },
-          });
         }
         if (properties.scale) {
-          options.scale = properties.scale.value as number;
-          live2d.scale(options.scale as number);
+          updateScale(properties.scale.value);
+          live2d.scale(properties.scale.value);
         }
       },
       applyGeneralProperties: async function (properties: Record<string, any>) {
+        if (properties.fps) {
+          updateMaxFPS(properties.fps as number);
+          live2d.app.ticker.maxFPS = properties.fps.value;
+        }
       },
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    live2d = new Live2DViewer(document.getElementById("canvas") as HTMLCanvasElement);
+  useEffect(() => {
+    (async () => {
+      if (Object.keys(models).length > 0) return;
 
-    Table.initialize("jp").then(() => {
-      fetchModels("./data/jp/Android/info.json").then(async (m) => {
-        models = m;
+      const initialize = async () => {
+        if (live2d !== undefined) return;
+        live2d = new Live2DViewer(document.getElementById("canvas") as HTMLCanvasElement);
+        await Table.initialize("jp");
+        const m = await fetchModels("./data/jp/Android/info.json");
+        setModels(m);
+      };
+      await initialize();
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [options.selectedModel]);
 
-        const base = get_origin();
-        for (const [k, v] of Object.entries(models)) {
-          models[k] = v.map((x) => base + "/" + x);
-        }
-
+  useEffect(() => {
+    (async () => {
+      const reload = async () => {
         const model = models[options.selectedModel as string];
-        const scale = options.scale as number;
-        live2d.playVoice = options.playVoice as boolean;
-        live2d.voiceVolume = options.voiceVolume as number;
-        live2d.app.ticker.maxFPS = options.maxFPS as number;
 
         for (let i = 0; i < model.length; i++) {
           const x = model[i];
@@ -98,21 +150,23 @@ export default function Home() {
         }
 
         live2d.center();
-        live2d.scale(scale);
-        live2d.playAnimation("Start_Idle_01", {
-          onComplete: () => {
-            if (live2d.getAnimation("Idle_01") == null) return;
+        live2d.start();
+      };
 
-            live2d.playAnimation("Idle_01");
-            live2d.loopAnimation = true;
-          },
-        });
-      });
-    });
-  });
+      if (Object.keys(models).length > 0) {
+        await reload();
+      }
+    })();
+  }, [options.selectedModel, models]);
+
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <canvas id="canvas">
+      <canvas
+        id="canvas"
+        onClick={() => {
+          if (options.tapToTalk) live2d.randomTalk();
+        }}
+      >
       </canvas>
     </div>
   );
